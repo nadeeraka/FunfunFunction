f360a65b0f0889a25aac4ceedb18ceb79ecd7bd4
nadeeraka <nimanthanadeeraka@outlook.com>
2018-12-02 18:23:20 +0530

    code added

--- factory_functions/ff.js
+++ factory_functions/ff.js
@@ -1,3 +1,15 @@
 // using factory function can reduse un nesasry complexcity
 //also if we use class base aproch  some times we have to deal with this
 // or bind.(this)
+
+const Dog = () => {
+  let sound = "woof";
+
+  return {
+    bark: () => console.log(sound)
+  };
+};
+
+let boxer = Dog();
+
+console.log(boxer.bark());
