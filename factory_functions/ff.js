6742ddf1d3fc2638bad31069d9a43c91b23c7434
nadeeraka <nimanthanadeeraka@outlook.com>
2018-12-02 18:33:25 +0530

    2

--- factory_functions/ff.js
+++ factory_functions/ff.js
@@ -1,6 +1,8 @@
 // using factory function can reduse un nesasry complexcity
 //also if we use class base aproch  some times we have to deal with this
-// or bind.(this)
+// or bind.(this)/
+// 0.00004 factory functions
+//0.00002 class
 
 const Dog = () => {
   let sound = "woof";
@@ -13,3 +15,14 @@ const Dog = () => {
 let boxer = Dog();
 
 console.log(boxer.bark());
+
+const round = radius => {
+  return {
+    radius,
+    draw: () => console.log("drawing round ...")
+  };
+};
+
+newRound = round();
+
+newRound.draw();
